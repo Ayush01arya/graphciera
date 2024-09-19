1911bc064ccca84748939abd152295f22f3a4668
Ayush Arya <69747175+Ayush01arya@users.noreply.github.com>
2024-09-19 12:39:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def upload_file():
         return jsonify({'error': 'An unexpected error occurred'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
